db2c44629af61e2eb4c5e9e7309f0c68185ba8cb
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2013-02-08 16:46:37 +0200

    Liikennepaikat-nappi lisätty ratapihaikkunaan

--- liikennepaikkadialogi.cpp
+++ liikennepaikkadialogi.cpp
@@ -1,11 +1,24 @@
 #include "liikennepaikkadialogi.h"
 #include "ui_liikennepaikkadialogi.h"
 
+#include <QSqlQuery>
+#include <QListWidgetItem>
+
 LiikennepaikkaDialogi::LiikennepaikkaDialogi(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LiikennepaikkaDialogi)
 {
     ui->setupUi(this);
+
+    // Haetaan luettelo
+    QSqlQuery kysely("select liikennepaikka, nimi from liikennepaikka order by nimi");
+    while( kysely.next())
+    {
+        QListWidgetItem* item = new QListWidgetItem(kysely.value(1).toString(), ui->liikennepaikkaLista);
+        item->setData(Qt::UserRole, kysely.value(0).toString());
+    }
+
+
 }
 
 LiikennepaikkaDialogi::~LiikennepaikkaDialogi()
